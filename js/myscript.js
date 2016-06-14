cf632bfbf3a35f253a61a5b6a2299d90d58084fa
Kelly Carmichael <digikidd@gmail.com>
2016-06-13 21:25:02 -0500

    no message

--- js/myscript.js
+++ js/myscript.js
@@ -1,45 +1,18 @@
 $(document).ready(function() {
+    
+    $('a[href^="#"]').on('click', function(event) {
 
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
+        var target = $( $(this).attr('href') );
 
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            // Prevent default anchor click behavior
+        if( target.length ) {
             event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 1000, function() {
-
-                // Add hash (#) to URL when done scrolling (default click behavior)
-                window.location.hash = hash;
-            });
-        } // End if
-
-              // clicking the "down" button will make the page scroll to the $elem's height
-    $('#nav_down').click(
-        function (e) {
-            $('html, body').animate({scrollTop: $elem.height()}, 800);
+                scrollTop: target.offset().top
+            }, 1000);
         }
-    );
-        // clicking the "up" button will make the page scroll to the top of the page
-    $('#nav_up').click(
-        function (e) {
-            $('html, body').animate({scrollTop: '0px'}, 800);
-        }
-    );
+
     });
 
-
-var infinite = new Waypoint.Infinite({
-  element: $('.infinite-container')[0]
-})
 
 
 });
